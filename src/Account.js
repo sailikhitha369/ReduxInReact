a6adf8bcff25f301afc8517379e2001660d715ba
sailikhitha369 <sailikitha48757@gmail.com>
2024-06-10 11:50:51 +0530

    first commit

---
+++ src/Account.js
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import store from "./store";
+
+function Account(){
+    let data=useSelector((state)=>{
+        return state;
+    })
+    console.log(data);
+    return(
+        <>
+        <div className="container">
+        <h2 className="text-primary">Account Details</h2>
+        <table className="table table-bordered w-50">
+          <thead>
+            <tr>
+              <th>Balance</th>
+              <th>user Name</th>
+              <th>Mobile</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>{data.balance}</td>
+              <td>{data.fullName}</td>
+              <td>{data.mobile}</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+        </>
+    )
+}
+export default Account;
